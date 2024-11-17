c2980bd530934e5cb4e47ba767e824a9dfb70587
whereisalidev <786syedalihussain@gmail.com>
2024-11-17 18:26:20 +0500

    updated

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 # app.config["SECRET_KEY"] = ""
 socketio = SocketIO(app)
 
-if __name__ == '__main__':
-    socketio.run(app)
 
 
-@app.route('/', ["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
 def home():
     return render_template('home.html')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
