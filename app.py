400af3ff060448d77b0163c33cf6477f277fa252
whereisalidev <786syedalihussain@gmail.com>
2024-11-18 18:04:49 +0500

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, render_template, session, url_for
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, join_room, leave_room
 from string import ascii_uppercase
 import random
 
@@ -52,10 +52,48 @@ def room():
 
 
 
-@socketio.on()
-def message():
-    pass
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return 
+    
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
 
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+    
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left the room {room}")
 
 
 
